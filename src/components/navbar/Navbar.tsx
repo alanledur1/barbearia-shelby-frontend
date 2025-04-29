641199c8eb3d067e073195fec1d7f7093aff0c5c
alanledur1 <ledur77conta2@gmail.com>
2025-04-29 20:34:08 -0300

    melhorias na estilização da navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import './Navbar.scss'; // importe o arquivo SCSS
+import { FaArrowRight } from "react-icons/fa6";
 
 export const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar__logo">SHELBY</div>
       <ul className="navbar__links">
-        <li><a href="#home">Início</a></li>
-        <li><a href="#sobre">Sobre</a></li>
-        <li><a href="#membros"></a></li>
+        <li className="navbar__links__dropdown">
+          <a href="#servicos">Serviços ▾</a>
+          <div className="navbar__links__dropdown__dropdown-content">
+            <a href="#barba">Barba</a>
+            <a href="#corte">Corte de cabelo</a>
+            <a href="#luzes">Luzes</a>
+          </div>
+        </li>
+        <li><a href="#sobre">Sobre Nós</a></li>
         <li><a href="#contato">Contato</a></li>
       </ul>
       <div className="navbar__containerButton">
         <p>Entrar</p>
-        <button className="button"></button>
+        <div className="container">
+          <p>Crie uma conta</p>
+          <button className="button">
+            <span className="icon"><FaArrowRight /></span>
+          </button>
+        </div>
       </div>
     </nav>
   );
