c6bf142805c7f8a5d6faf38e5308110ca9627153
alanledur1 <ledur77conta2@gmail.com>
2025-05-08 00:39:53 -0300

    modificações na navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,32 +4,34 @@ import { FaArrowRight } from "react-icons/fa6";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar__logo">
-        <img src="/images/logo.png" alt='Logo Shelby' className='navbar__logo-img' />
-        SHELBY
-      </div>
-      <ul className="navbar__links">
-        <li className="navbar__links__dropdown">
-          <a href="#servicos">Serviços ▾</a>
-          <div className="navbar__links__dropdown-content">
-            <a href="#barba">Barba</a>
-            <a href="#corte">Corte de cabelo</a>
-            <a href="#luzes">Luzes</a>
-          </div>
-        </li>
-        <li><a href="#sobre">Sobre Nós</a></li>
-        <li><a href="#contato">Contato</a></li>
-      </ul>
-      <div className="navbar__containerButton">
-        <p>Entrar</p>
-        <div className="container">
-          <p>Crie uma conta</p>
-          <button className="button">
-            <span className="icon"><FaArrowRight /></span>
-          </button>
+    <header>
+      <nav className="navbar" role="navigation" aria-label="Menu principal">
+        <div className="navbar__logo">
+          <img src="/images/logo.png" alt='Logo Shelby' className='navbar__logo-img' />
+          SHELBY
         </div>
-      </div>
-    </nav>
+        <ul className="navbar__links">
+          <li className="navbar__links__dropdown">
+            <a href="#servicos">Serviços ▾</a>
+            <div className="navbar__links__dropdown-content">
+              <a href="#barba">Barba</a>
+              <a href="#corte">Corte de cabelo</a>
+              <a href="#luzes">Luzes</a>
+            </div>
+          </li>
+          <li><a href="#sobre">Sobre Nós</a></li>
+          <li><a href="#contato">Contato</a></li>
+        </ul>
+        <div className="navbar__containerButton">
+          <button className="navbar__login">Entrar</button>
+          <div className="container">
+            <button className='navbar__signup'>Crie uma conta</button>
+            <button className="button">
+              <span className="icon"><FaArrowRight /></span>
+            </button>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
