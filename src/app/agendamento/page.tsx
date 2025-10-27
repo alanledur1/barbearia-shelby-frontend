fe86f55af250fd082473728643c6ae24f41fba61
alanledur1 <ledur77conta2@gmail.com>
2025-10-27 20:26:17 -0300

    fix: styles of page

--- src/app/agendamento/page.tsx
+++ src/app/agendamento/page.tsx
@@ -3,7 +3,6 @@
 import React, { useEffect, useState } from 'react';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
-import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useAuth } from '@/context/AuthContext';
